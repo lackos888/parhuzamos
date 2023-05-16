9a908cd383db19b80296704237825ded4d81ccd3
lackos888 <29657187+lackos888@users.noreply.github.com>
2023-05-16 08:42:16 +0200

    .
    .

--- main.c
+++ main.c
@@ -878,7 +878,7 @@ int main(void)
     /* 2x2-es példa END */
 
     /* 4x4-es példa */
-    int matrixSizes = 1536;
+    int matrixSizes = 1024;
 
     cl_long matrixAllocationSizes = matrixSizes * matrixSizes * sizeof(cl_long);
 
